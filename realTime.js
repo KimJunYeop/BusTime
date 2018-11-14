774d684655c287f9ed56353eabfbe688c03dd362
Kim Jun-Yeop <wnsduq8282@gmail.com>
2018-11-15 00:00:20 +0900

    초기단계

--- realTime.js
+++ realTime.js
@@ -1,17 +1,13 @@
-var timeLgToOsanWeekDay = [820, 910, 1010, 1110, 1210, 1240, 1330, 1410, 1510, 1730, 1745, 1800, 1845, 1900, 1915, 1930, 1945, 2000, 2015, 2030, 2045, 2100, 2115, 2145, 2200, 2215, 2225, 2245, 2315];
+var timeLgToOsanWeekDay = [820, 910, 1010, 1110, 1210, 1240, 1330, 1410, 1510, 1730, 1745, 1800, 1815, 1830, 1845, 1900, 1915, 1930, 1945, 2000, 2015, 2030, 2045, 2100, 2130, 2145, 2200, 2210, 2230, 2300, 2310];
 var timeOasnToLgWeekDay = [640, 710, 725, 840, 925, 1025, 1125, 1225, 1255, 1345, 1425, 1525, 1815, 1830, 1845, 1900, 1915, 1930, 1945, 2000, 2015, 2030, 2045, 2100, 2115, 2145, 2200, 2215, 2225, 2245, 2315];
 
-var timeLgToOsanWeekend = [910, 1010, 1110, 1210, 1240, 1330, 1410, 1510, 1610, 1730, 1800, 1830, 1900, 1930, 2000, 2030, 2100, 2115, 2130, 2145, 2200, 2215, 2215, 2230, 2245, 2300];
+var timeLgToOsanWeekend = [910, 1010, 1110, 1210, 1240, 1330, 1410, 1510, 1610, 1730, 1800, 1830, 1900, 1930, 2000, 2030, 2100, 2115, 2130, 2145, 2200, 2215, 2230, 2245, 2300];
 var timeOsanToLgWeekend = [840, 925, 1025, 1125, 1225, 1255, 1345, 1425, 1525, 1745, 1815, 1845, 1915, 1945, 2015, 2045, 2115, 2130, 2145, 2200, 2215, 2230, 2245, 2300, 2315];
-
-var count = 0;
 
 var _realTimeInterval;
 
 $(document).ready(function () {
-    console.log('hello');
     realTimeBind();
-
 })
 
 function realTimeBind() {
@@ -20,11 +16,13 @@ function realTimeBind() {
     })
 
     $("#osanToLg").unbind().bind('click', function () {
+        console.log('osanToLg')
         clearInterval(_realTimeInterval);
         _realTimeInterval = setInterval(displayTimeOsanToLg, 1000);
     })
 
     $("#lgToOsan").unbind().bind('click', function () {
+        console.log('lgToOsan');
         clearInterval(_realTimeInterval);
         _realTimeInterval = setInterval(displayTimeLgToOsan, 1000);
     });
@@ -33,282 +31,75 @@ function realTimeBind() {
 
 function displayTimeOsanToLg() {
     // 일  0 월  1 화  2 수  3 목  4 금  5 토  6
-    var weekend;
     var day = new Date();
-    var year = day.getFullYear();
-    var month = day.getMonth();
-    var today = day.getDate();
-    var hour = day.getHours();
-    var minute = day.getMinutes();
-    var vindex = -1;
-    var currentTime = hour.toString() + minute.toString();
-
-    var findHour;
-    var fineMinute;
-    var findIndex;
-
     if (day.getDay() == 0 || day.getDay() == 6) {
-        weekend = true;
+        displayTimeModule(timeOsanToLgWeekend);
     } else {
-        weekend = false;
-    }
-
-    currentTime = 2313;
-
-    if (weekend) {
-        timeOsanToLgWeekend.every(function (value, index) {
-            // Do your thing, then:
-            if (currentTime <= value) {
-                console.log(value);
-                findIndex = index;
-                return false;
-            } else {
-                return true;
-            }
-        })
-        console.log(findIndex);
-
-        var findTime = timeOsanToLgWeekend[findIndex].toString();
-
-        if (findTime.lenght == 3) {
-            findHour = findTime.substr(0, 1);
-            findMinute = findTime.substr(1, 3);
-        } else {
-            findHour = findTime.substr(0, 2);
-            findMinute = findTime.substr(2, 4);
-        }
-
-        var findDate = new Date(year, month, today, findHour, findMinute, 0);
-        var testDate = new Date(year, month, today, 23, 13, 0);
-        // var toDate = new Date();
-        // console.log(findDate);
-        // console.log(testDate);
-        var diffTimeSecond = Math.floor((findDate.getTime() - (testDate.getTime())) / 1000);
-        var diffTimeMinute = Math.floor(diffTimeSecond / 60);
-        diffTimeSecond = diffTimeSecond - diffTimeMinute * 60;
-
-        // console.log(diffTimeMinute);
-        // console.log(diffTimeSecond);
-
-        if (diffTimeSecond < 0) {
-            console.log('버스가 출발했어요 (혹은 막차여서 택시타야되요.)');
-        } else {
-            $("#timeDisplay").html(diffTimeMinute + '분 ');
-            $("#timeDisplay").append(diffTimeSecond + '초 남았습니다');
-        }
-
-
-    } else {
-        timeOasnToLgWeekDay.every(function (value, index) {
-            // Do your thing, then:
-            if (currentTime <= value) {
-                console.log(value);
-                findIndex = index;
-                return false;
-            } else {
-                return true;
-            }
-        })
-
-        var findTime = timeOasnToLgWeekDay[findIndex].toString();
-
-        if (findTime.lenght == 3) {
-            findHour = findTime.substr(0, 1);
-            findMinute = findTime.substr(1, 3);
-        } else {
-            findHour = findTime.substr(0, 2);
-            findMinute = findTime.substr(2, 4);
-        }
-
-        var findDate = new Date(year, month, today, findHour, findMinute, 0);
-        var testDate = new Date(year, month, today, 23, 13, 0);
-        // var toDate = new Date();
-        // console.log(findDate);
-        // console.log(testDate);
-        var diffTimeSecond = Math.floor((findDate.getTime() - (testDate.getTime())) / 1000);
-        var diffTimeMinute = Math.floor(diffTimeSecond / 60);
-        diffTimeSecond = diffTimeSecond - diffTimeMinute * 60;
-
-        // console.log(diffTimeMinute);
-        // console.log(diffTimeSecond);
-
-        if (diffTimeSecond < 0) {
-            console.log('버스가 출발했어요 (혹은 막차여서 택시타야되요.)');
-        } else {
-            $("#timeDisplay").html(diffTimeMinute + '분 ');
-            $("#timeDisplay").append(diffTimeSecond + '초 남았습니다');
-        }
+        displayTimeModule(timeOasnToLgWeekDay);
     }
 }
-
 
 function displayTimeLgToOsan() {
     // 일  0 월  1 화  2 수  3 목  4 금  5 토  6
-    var weekend;
+    var day = new Date();
+
+    if (day.getDay() == 0 || day.getDay() == 6) {
+        displayTimeModule(timeLgToOsanWeekend);
+    } else {
+        displayTimeModule(timeLgToOsanWeekDay);
+    }
+}
+
+
+function displayTimeModule(busTimeArray) {
     var day = new Date();
     var year = day.getFullYear();
     var month = day.getMonth();
     var today = day.getDate();
     var hour = day.getHours();
     var minute = day.getMinutes();
-    var vindex = -1;
-    var currentTime = hour.toString() + minute.toString();
-
+    var currentTime;
     var findHour;
-    var fineMinute;
-    var findIndex;
+    var findTime = -1;
+    var findDate;
+    var todayDate;
+    var diffTimeSecond;
+    var diffTimeMinute;
+    var findValueArray;
 
-    if (day.getDay() == 0 || day.getDay() == 6) {
-        weekend = true;
+    if (minute < 10) {
+        currentTime = hour.toString() + '0' + minute.toString();
     } else {
-        weekend = false;
+        currentTime = hour.toString() + minute.toString();
     }
 
-    currentTime = 10000;
+    findValueArray = busTimeArray.filter(val => { return val >= currentTime });
 
-    if (weekend) {
-        timeLgToOsanWeekend.every(function (value, index) {
-            // Do your thing, then:
-            if (currentTime <= value) {
-                console.log(value);
-                findIndex = index;
-                return false;
-            } else {
-                return true;
-            }
-        })
-        console.log(findIndex);
+    if (findValueArray.length != 0) {
+        findTime = findValueArray[0].toString();
+    }
 
-        var findTime = timeLgToOsanWeekend[findIndex].toString();
-
-        if (findTime.lenght == 3) {
-            findHour = findTime.substr(0, 1);
-            findMinute = findTime.substr(1, 3);
-        } else {
-            findHour = findTime.substr(0, 2);
-            findMinute = findTime.substr(2, 4);
-        }
-
-        var findDate = new Date(year, month, today, findHour, findMinute, 0);
-        var testDate = new Date(year, month, today, 23, 13, 0);
-        // var toDate = new Date();
-        // console.log(findDate);
-        // console.log(testDate);
-        var diffTimeSecond = Math.floor((findDate.getTime() - (testDate.getTime())) / 1000);
-        var diffTimeMinute = Math.floor(diffTimeSecond / 60);
-        diffTimeSecond = diffTimeSecond - diffTimeMinute * 60;
-
-        // console.log(diffTimeMinute);
-        // console.log(diffTimeSecond);
-
-        if (diffTimeSecond < 0) {
-            console.log('버스가 출발했어요 (혹은 막차여서 택시타야되요.)');
-        } else {
-            $("#timeDisplay").html(diffTimeMinute + '분 ');
-            $("#timeDisplay").append(diffTimeSecond + '초 남았습니다');
-        }
-
-
+    if (findTime == -1) {
+        $("#timeDisplay").html('막차가 끊겼습니다.');
+        return false;
+    } else if (findTime.length == 3) {
+        findHour = findTime.substr(0, 1);
+        findMinute = findTime.substr(1, 3);
     } else {
-        timeLgToOsanWeekDay.every(function (value, index) {
-            // Do your thing, then:
-            if (currentTime <= value) {
-                console.log(value);
-                findIndex = index;
-                return false;
-            } else {
-                return true;
-            }
-        })
-        console.log(findIndex);
-        if(findIndex == undefined) {
-            findIndex = 0;
-        }
-        var findTime = timeLgToOsanWeekDay[findIndex].toString();
-        console.log(findTime);
-        if (findTime.lenght == 3) {
-            findHour = findTime.substr(0, 1);
-            findMinute = findTime.substr(1, 3);
-        } else {
-            findHour = findTime.substr(0, 2);
-            findMinute = findTime.substr(2, 4);
-        }
+        findHour = findTime.substr(0, 2);
+        findMinute = findTime.substr(2, 4);
+    }
 
-        var findDate = new Date(year, month, today, findHour, findMinute, 0);
-        var testDate = new Date(year, month, today, 23, 13, count++);
-        // var toDate = new Date();
-        // console.log(findDate);
-        // console.log(testDate);
-        var diffTimeSecond = Math.floor((findDate.getTime() - (testDate.getTime())) / 1000);
-        var diffTimeMinute = Math.floor(diffTimeSecond / 60);
-        diffTimeSecond = diffTimeSecond - diffTimeMinute * 60;
+    findDate = new Date(year, month, today, findHour, findMinute, 0);
+    todayDate = new Date();
+    diffTimeSecond = Math.floor((findDate.getTime() - (todayDate.getTime())) / 1000);
+    diffTimeMinute = Math.floor(diffTimeSecond / 60);
+    diffTimeSecond = diffTimeSecond - diffTimeMinute * 60;
 
-        // console.log(diffTimeMinute);
-        // console.log(diffTimeSecond);
-
-        if (diffTimeSecond < 0) {
-            console.log('버스가 출발했어요 (혹은 막차여서 택시타야되요.)');
-        } else {
-            $("#timeDisplay").html(diffTimeMinute + '분 ');
-            $("#timeDisplay").append(diffTimeSecond + '초 남았습니다');
-        }
+    if (diffTimeMinute < 0) {
+        $("#timeDisplay").html('버스가 출발했어요. 다음차를 이용하세요.');
+    } else {
+        $("#timeDisplay").html(diffTimeMinute + '분 ');
+        $("#timeDisplay").append(diffTimeSecond + '초 남았습니다');
     }
 }
-
-
-// function displayTimeOsanToLg() {
-//     //일  0 월  1 화  2 수  3 목  4 금  5 토  6
-//     var weekend;
-//     var day = new Date();
-//     var year = day.getFullYear();
-//     var month = day.getMonth();
-//     var today = day.getDate();
-//     var hour = day.getHours();
-//     var minute = day.getMinutes();
-//     var vindex = -1;
-//     var aa = hour.toString() + minute.toString();
-
-//     if (day.getDay() == 0 || day.getDay() == 6) {
-//         weekend = true;
-//     } else {
-//         weekend = false;
-//     }
-
-//     if (weekend) {
-//         timeLgToOsanWeekend.some(function (value, index) {
-//             if (Number(aa) < value) {
-//                 vindex = index;
-//             }
-//             return (Number(aa) < value);
-//         })
-//         if (timeLgToOsanWeekend[vindex].toString().length == 3) {
-//             var timeNextHour = timeLgToOsanWeekend[vindex].toString().substr(0, 1);
-//             var timeNextMinute = timeLgToOsanWeekend[vindex].toString().substr(1, 2);
-//         } else {
-//             var timeNextHour = timeLgToOsanWeekend[vindex].toString().substr(0, 2);
-//             var timeNextMinute = timeLgToOsanWeekend[vindex].toString().substr(2, 2);
-//         }
-//         var nextDate = new Date(year, month, today, timeNextHour, timeNextMinute, 0);
-//     } else {
-//         timeLgToOsanWeekDay.some(function (value, index) {
-//             if (Number(aa) < value) {
-//                 vindex = index;
-//             }
-//             return (Number(aa) < value);
-//         })
-//         if (timeLgToOsanWeekDay[vindex].toString().length == 3) {
-//             var timeNextHour = timeLgToOsanWeekDay[vindex].toString().substr(0, 1);
-//             var timeNextMinute = timeLgToOsanWeekDay[vindex].toString().substr(1, 2);
-//         } else {
-//             var timeNextHour = timeLgToOsanWeekDay[vindex].toString().substr(0, 2);
-//             var timeNextMinute = timeLgToOsanWeekDay[vindex].toString().substr(2, 2);
-//         }
-//         var nextDate = new Date(year, month, today, timeNextHour, timeNextMinute, 0);
-//     }
-//     var diffTime = Math.floor((nextDate.getTime() - (new Date().getTime())) / 1000);
-//     var diffTimeMinute = Math.floor(diffTime / 60);
-//     // console.log(diffTimeMinute)
-//     //3600으로 나누면 이게 초다
-//     $("#timeDisplay").html(diffTimeMinute + '분 ');
-//     $("#timeDisplay").append((diffTime - diffTimeMinute * 60) + '초 남았습니다');
-// }
--- totalTime.js
+++ totalTime.js
@@ -1,10 +1,62 @@
-$(document).ready(function(){
+var timeLgToOsanWeekDay = [820, 910, 1010, 1110, 1210, 1240, 1330, 1410, 1510, 1730, 1745, 1800, 1815, 1830, 1845, 1900, 1915, 1930, 1945, 2000, 2015, 2030, 2045, 2100, 2130, 2145, 2200, 2210, 2230, 2300, 2310];
+var timeOasnToLgWeekDay = [640, 710, 725, 840, 925, 1025, 1125, 1225, 1255, 1345, 1425, 1525, 1815, 1830, 1845, 1900, 1915, 1930, 1945, 2000, 2015, 2030, 2045, 2100, 2115, 2145, 2200, 2215, 2225, 2245, 2315];
 
+var timeLgToOsanWeekend = [910, 1010, 1110, 1210, 1240, 1330, 1410, 1510, 1610, 1730, 1800, 1830, 1900, 1930, 2000, 2030, 2100, 2115, 2130, 2145, 2200, 2215, 2230, 2245, 2300];
+var timeOsanToLgWeekend = [840, 925, 1025, 1125, 1225, 1255, 1345, 1425, 1525, 1745, 1815, 1845, 1915, 1945, 2015, 2045, 2115, 2130, 2145, 2200, 2215, 2230, 2245, 2300, 2315];
+
+$(document).ready(function () {
+    console.log('timeLgToOsanWeekDay length : ', timeLgToOsanWeekDay.length);
+    console.log('timeOasnToLgWeekDay length : ', timeOasnToLgWeekDay.length);
+    console.log('timeLgToOsanWeekend length : ', timeLgToOsanWeekend.length);
+    console.log('timeOsanToLgWeekend length : ', timeOsanToLgWeekend.length);
+    totalTimeInit();
     totalTimeBind();
 });
 
+function totalTimeInit() {
+    var day = new Date().getDay();
+    if(day == 0 || day == 6) {
+        displayTable(2)
+    } else {
+        displayTable(1)
+    }
+}
+
 function totalTimeBind() {
-    $("#backHome").unbind().bind('click',function() {
+    $("#backHome").unbind().bind('click', function () {
         window.location.href = './index.html';
     });
+
+    $("#week").unbind().bind('click', function () {
+        $('#weekTable > tbody').empty();
+        displayTable(1);
+    })
+
+    $("#weekend").unbind().bind('click', function () {
+        $('#weekTable > tbody').empty();
+        displayTable(2);
+    })
+}
+
+function displayTable(num) {
+    var html = '';
+    if(num == 1) {
+        for (var i = 0; i < 31; i++) {
+            html += '<tr>';
+            html += '<td>'+timeOasnToLgWeekDay[i]+'</td>'
+            html += '<td>'+timeLgToOsanWeekDay[i]+'</td>';
+            html += '</tr>'
+            $('#weekTable > tbody:last').append(html);
+            html = '';
+        }
+    } else if(num == 2) {
+        for (var i = 0; i < 25; i++) {
+            html += '<tr>';
+            html += '<td>'+timeOsanToLgWeekend[i]+'</td>'
+            html += '<td>'+timeLgToOsanWeekend[i]+'</td>';
+            html += '</tr>'
+            $('#weekTable > tbody:last').append(html);
+            html = '';
+        }
+    }
 }
